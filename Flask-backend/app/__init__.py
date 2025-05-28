33fff2756794e654b4bb913816baec933094c3f5
mrbear <mrbear@DESKTOP-V64KSDE.localdomain>
2025-05-27 22:03:36 -0500

    Remove Flask login for gwt auth

--- Flask-backend/app/__init__.py
+++ Flask-backend/app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_migrate import Migrate
-from flask_login import LoginManager
-from app.db import db  
+from app.db import db
 from config import config
 from flask_cors import CORS
 
@@ -18,25 +17,11 @@ def create_app(config_name="default"):
     db.init_app(app)
     Migrate(app, db)
 
-    # Initialize Flask-Login
-    login_manager = LoginManager()
-    login_manager.init_app(app)
-    login_manager.login_view = "login"  # Redirect to 'login' route if not authenticated
-    login_manager.login_message = "Please log in to access this page."
-    login_manager.login_message_category = "info"
-
-    # User loader callback
-    from app.models import User
-
-    @login_manager.user_loader
-    def load_user(user_id):
-        return db.session.get(User, int(user_id))
-
-    # Register blueprints/routes
+    # Register routes
     from app.routes import init_routes
     init_routes(app)
 
-    # Inject year into templates
+    # Add year into templates
     @app.context_processor
     def inject_year():
         from datetime import datetime
